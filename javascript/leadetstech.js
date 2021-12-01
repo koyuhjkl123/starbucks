c2f6aeb4442de487a0781c6d57af90c4b98a6d25
koyuhjkl123 <koyuhjkl123@naver.com>
2021-12-01 21:32:54 +0900

    벳지 이미지 수정

---
+++ javascript/leadetstech.js
@@ -0,0 +1,19 @@
+
+// new Swiper('.main_section .section-dong', {
+//     // direction: 'horizontal', // 수평 슬라이드
+//        slidesPerView: 5, // 한번에 보여줄 슬라이드 개수
+//        spaceBetween: 10, // 슬라이드 사이 여백
+//        centeredSlides: true, // 1번 슬라이드가 가운데 보이기 
+//        loop:true,
+//        autoplay: {
+//            delay: 10000 //1초 자동 슬라이드
+//        },
+//        pagination: {
+//            el: '.promotion .swiper-pagination', // 페이지번호 요소 선택자
+//            clickable: true // 사용자의 페이지 번호 요소 제어
+//        },
+//       navigation: {
+//           prevEl: '.main_section .swiper-prev',
+//           nextEl: '.main_section .swiper-next'
+//       }
+// });
